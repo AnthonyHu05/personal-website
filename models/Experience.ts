7b42bdf076b4432a59307cc90e2903eeb4986b5c
Anthony Hu <a9hu@ucsd.edu>
2025-12-15 00:15:07 +0800

    Finish task 2 setup database and schemas

---
+++ models/Experience.ts
@@ -0,0 +1,31 @@
+import { HydratedDocument, InferSchemaType, Model, Schema, model, models } from "mongoose";
+
+const transformDocument = (_: unknown, ret: Record<string, unknown>) => {
+    ret.id = ret._id?.toString();
+    delete ret._id;
+    return ret;
+};
+
+const experienceSchema = new Schema(
+    {
+        company: { type: String, required: true },
+        title: { type: String, required: true },
+        location: { type: String, required: true },
+        startDate: { type: Date, required: true },
+        endDate: { type: Date, required: false }, 
+        description: { type: String, required: true },
+    },
+    {
+        toJSON: { virtuals: true, versionKey: false, transform: transformDocument },
+        toObject: { virtuals: true, versionKey: false, transform: transformDocument },
+    },
+);
+
+export type ExperienceInput = InferSchemaType<typeof experienceSchema>;
+export type Experience = ExperienceInput & { id: string };
+export type ExperienceDocument = HydratedDocument<ExperienceInput>;
+
+const ExperienceModel: Model<ExperienceInput> =
+    (models.Experience as Model<ExperienceInput>) || model<ExperienceInput>("Experience", experienceSchema);
+
+export default ExperienceModel;
---
+++ models/Project.ts
@@ -0,0 +1,31 @@
+import { HydratedDocument, InferSchemaType, Model, Schema, model, models } from "mongoose";
+
+const transformDocument = (_: unknown, ret: Record<string, unknown>) => {
+    ret.id = ret._id?.toString();
+    delete ret._id;
+    return ret;
+};
+
+const projectSchema = new Schema(
+    {
+        projectName: { type: String, required: true },
+        startDate: { type: Date, required: true },
+        endDate: { type: Date, required: false }, 
+        description: { type: String, required: true },
+        deploymentLink: { type: String, required: false }, 
+        githubLink: { type: String, required: false }, 
+    },
+    {
+        toJSON: { virtuals: true, versionKey: false, transform: transformDocument },
+        toObject: { virtuals: true, versionKey: false, transform: transformDocument },
+    },
+);
+
+export type ProjectInput = InferSchemaType<typeof projectSchema>;
+export type Project = ProjectInput & { id: string };
+export type ProjectDocument = HydratedDocument<ProjectInput>;
+
+const ProjectModel: Model<ProjectInput> =
+    (models.Project as Model<ProjectInput>) || model<ProjectInput>("Project", projectSchema);
+
+export default ProjectModel;
