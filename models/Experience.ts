091f9fed29b7dec059f5bc2be01fdfbcf2c5eb45
Anthony Hu <a9hu@ucsd.edu>
2025-12-15 16:29:08 +0800

    add css into global.css

--- models/Experience.ts
+++ models/Experience.ts
@@ -1,24 +1,35 @@
-import { HydratedDocument, InferSchemaType, Model, Schema, model, models } from "mongoose";
+import {
+  HydratedDocument,
+  InferSchemaType,
+  Model,
+  Schema,
+  model,
+  models,
+} from "mongoose";
 
 const transformDocument = (_: unknown, ret: Record<string, unknown>) => {
-    ret.id = ret._id?.toString();
-    delete ret._id;
-    return ret;
+  ret.id = ret._id?.toString();
+  delete ret._id;
+  return ret;
 };
 
 const experienceSchema = new Schema(
-    {
-        company: { type: String, required: true },
-        title: { type: String, required: true },
-        location: { type: String, required: true },
-        startDate: { type: Date, required: true },
-        endDate: { type: Date, required: false }, 
-        description: { type: String, required: true },
+  {
+    company: { type: String, required: true },
+    title: { type: String, required: true },
+    location: { type: String, required: true },
+    startDate: { type: Date, required: true },
+    endDate: { type: Date, required: false },
+    description: { type: String, required: true },
+  },
+  {
+    toJSON: { virtuals: true, versionKey: false, transform: transformDocument },
+    toObject: {
+      virtuals: true,
+      versionKey: false,
+      transform: transformDocument,
     },
-    {
-        toJSON: { virtuals: true, versionKey: false, transform: transformDocument },
-        toObject: { virtuals: true, versionKey: false, transform: transformDocument },
-    },
+  }
 );
 
 export type ExperienceInput = InferSchemaType<typeof experienceSchema>;
@@ -26,6 +37,7 @@ export type Experience = ExperienceInput & { id: string };
 export type ExperienceDocument = HydratedDocument<ExperienceInput>;
 
 const ExperienceModel: Model<ExperienceInput> =
-    (models.Experience as Model<ExperienceInput>) || model<ExperienceInput>("Experience", experienceSchema);
+  (models.Experience as Model<ExperienceInput>) ||
+  model<ExperienceInput>("Experience", experienceSchema);
 
 export default ExperienceModel;
